c21fecf294d0614db1c7232a41a9f89cc8201f7f
MohitBhalotia <158545409+MohitBhalotia@users.noreply.github.com>
2024-12-19 13:48:22 +0000

    Comparison done

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true - comparision convert karta hai issliye wo 0 mein convert ho jaata hai
+
+console.log(undefined == 0); //false
+console.log(undefined > 0);  //false
+console.log(undefined < 0);  //false
+
+// === 
+
+console.log("2" === 2);
