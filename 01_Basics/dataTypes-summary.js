bedfb9be91363f014663db533b7ccf836918654c
MohitBhalotia <158545409+MohitBhalotia@users.noreply.github.com>
2024-12-19 14:34:58 +0000

    Stack and Heap done

--- 01_Basics/dataTypes-summary.js
+++ 01_Basics/dataTypes-summary.js
@@ -2,37 +2,35 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
-
-
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
@@ -52,3 +50,24 @@ Return type of variables in JavaScript
        Function  =>  function
        Object  =>  object
 */
+
+// ++++++++++++++++++++++++++++++
+
+// Stack(Primitive) and Heap(Non-Primitive)
+let myYoutubename = "hiteshchoudharydotcom";
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+  email: "user@google.com",
+  number: "12345678",
+};
+let userTwo = userOne;
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
